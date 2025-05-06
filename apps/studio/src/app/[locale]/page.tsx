410a7b8dabbb1fb696eb24115b69f6cce2a3572e
piro0919 <piro.haniwa@gmail.com>
2025-05-07 05:04:19 +0900

    feat: add prisma setup and update dependencies across the project

--- apps/studio/src/app/[locale]/page.tsx
+++ apps/studio/src/app/[locale]/page.tsx
@@ -1,3 +1,10 @@
-export default function Page(): React.JSX.Element {
+import prisma from "@prisma/client";
+
+export default async function Page(): Promise<React.JSX.Element> {
+  const prismaClient = new prisma.PrismaClient();
+  const user = await prismaClient.user.findFirst();
+
+  console.log(user);
+
   return <div>studio</div>;
 }
